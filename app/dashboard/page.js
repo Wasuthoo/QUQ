c2e5d8c63f4a650d61372d573e9c00ef6ab01886
Wasu <67475566+Wasuthoo@users.noreply.github.com>
2023-10-06 18:53:50 +0700

    init

---
+++ app/dashboard/page.js
@@ -0,0 +1,73 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+
+const QueueManagement = () => {
+  const [queue, setQueue] = useState([]);
+  const [calledPerson, setCalledPerson] = useState('');
+  const [ws, setWs] = useState(null);
+
+  useEffect(() => {
+    const newWs = new WebSocket('ws://localhost:8000');
+
+    newWs.onopen = () => {
+      console.log('WebSocket connection opened');
+    };
+
+    newWs.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      console.log('Received from server:', data);
+
+      if (Array.isArray(data)) {
+        // Received a new queue
+        setQueue(data);
+      } else {
+        // Received a called person
+        setQueue(data.queue);
+        setCalledPerson(data.calledPerson);
+      }
+    };
+
+    newWs.onclose = () => {
+      console.log('WebSocket connection closed');
+    };
+
+    setWs(newWs);
+
+    return () => {
+      newWs.close();
+    };
+  }, []);
+
+  const addToQueue = () => {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      ws.send('add'); // Send the string message 'add'
+    } else {
+      console.error('WebSocket not open or not initialized.');
+    }
+  };
+  
+  const callNextInQueue = () => {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      ws.send('call'); // Send the string message 'call'
+    } else {
+      console.error('WebSocket not open or not initialized.');
+    }
+  };
+  
+
+  return (
+    <div>
+      <h1 className='font-bold'>DASHBOARD</h1>
+
+      <h2>Queue</h2>
+      <ul>
+        {queue.map((person, index) => (
+          <li key={index}>{person}</li>
+        ))}
+      </ul>
+      {calledPerson && <p>Called: {calledPerson}</p>}
+    </div>
+  );
+};
+
+export default QueueManagement;
---
+++ app/page.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from 'react';
+import QueueManagement from '../components/QueueManagement';
+
+const HomePage = () => {
+  return (
+    <div>
+      <QueueManagement />
+    </div>
+  );
+};
+
+export default HomePage;
